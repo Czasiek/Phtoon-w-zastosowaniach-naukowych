0527f41292413de96af7966c9cf565b76c838150
Czasiek <93408115+Czasiek@users.noreply.github.com>
2023-01-12 19:37:30 +0100

    Add files via upload

---
+++ lab6/lab6.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from tqdm import tqdm
+import imageio.v2 as imageio
+from numba import jit
+
+class IsingModel:
+    def __init__(self, size, temp):
+        self.size = size
+        self.temp = temp
+        self.lattice = np.random.choice([-1, 1], size=(size, size))
+
+    def energy(self, i, j):
+        return - self.lattice[i, j] * (self.lattice[i, (j+1)%self.size] + self.lattice[i, (j-1)%self.size] + self.lattice[(i+1)%self.size, j] + self.lattice[(i-1)%self.size, j])
+
+    @jit
+    def monte_carlo(self, steps):
+        for step in tqdm(range(steps)):
+            i, j = np.random.randint(0, self.size, 2)
+            delta_e = 2 * self.lattice[i, j] * self.energy(i, j)
+            if delta_e <= 0 or np.random.random() < np.exp(-delta_e / self.temp):
+                self.lattice[i, j] *= -1
+            self.save_lattice(step)
+            self.save_magnetization(step)
+
+    def save_lattice(self, step):
+        plt.imshow(self.lattice, cmap='coolwarm')
+        plt.title(f'Step {step}')
+        plt.savefig(f'step{step}.png')
+
+    def save_magnetization(self, step):
+        magnetization = np.sum(self.lattice) / (self.size * self.size)
+        with open(f'magnetization{step}.txt', 'w') as f:
+            f.write(str(magnetization))
+
+    def create_gif(self):
+        images = []
+        for i in range(steps):
+            images.append(imageio.imread(f'step{i}.png'))
+        imageio.mimsave('ising_simulation.gif', images)
+
+# usage 
+size = 3
+temp = 2.27
+steps = 10
+
+simulation = IsingModel(size, temp)
+
+# without numba
+import time
+start_time = time.time()
+simulation.monte_carlo(steps)
+end_time = time.time()
+print("Without Numba: ", end_time - start_time)
+
+# with numba
+start_time = time.time()
+simulation.monte_carlo(steps)
+end_time = time.time()
+print("With Numba: ", end_time - start_time)
+
+simulation.create_gif()
